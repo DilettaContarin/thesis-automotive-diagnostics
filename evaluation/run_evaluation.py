f2e002adc78d3df08ca93b0e546e2fb2bbf5bf55
DilettaContarin <dilettacontarin@gmail.com>
2026-06-18 19:25:55 +0200

    Add sys.path modification for module imports

--- evaluation/run_evaluation.py
+++ evaluation/run_evaluation.py
@@ -22,6 +22,8 @@ Usage:
         --latency       data/latency_records.json \
         --vehicle       punto
 """
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'online'))
 
 import json
 import os
